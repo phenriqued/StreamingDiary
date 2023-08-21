7e2e6de272287be7831ef82a562fe7022af5f14c
Pedro <pedrodias3737@gmail.com>
2023-08-21 09:53:20 -0300

    Piloto

---
+++ src/main/java/phenriqued/com/github/StreamingDiary/StreamingDiaryApplication.java
@@ -0,0 +1,13 @@
+package phenriqued.com.github.StreamingDiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/phenriqued/com/github/StreamingDiary/StreamingDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package phenriqued.com.github.StreamingDiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
